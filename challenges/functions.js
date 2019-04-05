2e8a057bd21ace88bf2c7ac79515dffe2fb4bd65
EoPlex <you@example.com>
2019-04-05 10:15:40 -0700

    stuck on functions.js, moving on

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,36 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(alpha, beta, cb) {
+  return cb(alpha, beta);
+}
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(alpha, beta, cb) {
+  return cb(alpha + beta);
+}
+
+
+function multiply(alpha, beta, cb) {
+  return cb(alpha * beta)
+}
+
+
+function greeting(alpha, beta, cb) {
+  return cb('Hello' + alpha + beta + ', nice to meet you!')
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +44,12 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+/* 
+
+`nestedfunction()` can access the variable `internal` because 'internal' 
+ is declared in the lexical environment of 'nestedFunction().
+
+ */
 
 
 const external = "I'm outside the function";
